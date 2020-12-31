68d0b3a917fe4dc3de7214e1fa6fd0bff823f75c
saurabh080200-tech <69123704+saurabh080200-tech@users.noreply.github.com>
2020-12-31 21:17:18 +0530

    Add files via upload

---
+++ HSV_value_detector.py
@@ -0,0 +1,46 @@
+# This program helps to detect the lower HSV value for a particular colour
+
+import cv2
+import numpy as np
+
+cap = cv2.imread('new3.jpeg')
+
+def nothing(x):
+    pass
+# Creating a window for later use
+cv2.namedWindow('result')
+
+# Starting with 100's to prevent error while masking
+h,s,v = 100,100,100
+
+# Creating track bar
+cv2.createTrackbar('h', 'result',0,179,nothing)
+cv2.createTrackbar('s', 'result',0,255,nothing)
+cv2.createTrackbar('v', 'result',0,255,nothing)
+
+while(1):
+
+    #converting to HSV
+    hsv = cv2.cvtColor(cap,cv2.COLOR_BGR2HSV)
+
+    # get info from track bar and appy to result
+    h = cv2.getTrackbarPos('h','result')
+    s = cv2.getTrackbarPos('s','result')
+    v = cv2.getTrackbarPos('v','result')
+
+    # Normal masking algorithm
+    lower_blue = np.array([h,s,v])
+    upper_blue = np.array([180,255,255])
+
+    mask = cv2.inRange(hsv,lower_blue, upper_blue)
+
+    result = cv2.bitwise_and(cap,cap,mask = mask)
+
+    cv2.imshow('result',result)
+
+    if cv2.waitKey(3) == ord('q'):
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
